5c99208730dbcf307258fba0ccf3ccbb39b68727
Isaac A. Murchie <isaac.appium@gmail.com>
2019-06-20 10:23:05 -0400

    Bring syslog specs into the fold (#13)

--- test/syslog/syslog-service-specs.js
+++ test/syslog/syslog-service-specs.js
@@ -1,42 +1,28 @@
-import { fs } from 'appium-support';
-import path from 'path';
-import net from 'net';
 import B from 'bluebird';
-
 import chai from 'chai';
-import chaiAsPromised from 'chai-as-promised';
 import SyslogService from '../../lib/syslog/syslog-service';
+import { getServerWithFixtures, fixtures } from '../fixtures';
 
 
 chai.should();
-chai.use(chaiAsPromised);
-
-let syslogFixtures;
 
 describe('syslog', function () {
-  before(async function () {
-    const syslogMessages = path.resolve(__dirname, '..', '..', '..', 'test', 'fixtures', 'syslogmessages.bin');
-    syslogFixtures = await fs.readFile(syslogMessages);
-  });
-  beforeEach(function () {
-    this.server = net.createServer();
-    this.server.listen();
-    let socket = net.connect(this.server.address());
-    this.syslogService = new SyslogService(socket);
-  });
+  let server;
+  let socket;
+  let syslogService;
+
   afterEach(function () {
-    this.syslogService.close();
-    this.server.close();
+    syslogService.close();
+    if (server) {
+      server.close();
+    }
   });
 
   it('should wait for first syslog message', async function () {
-    this.server.on('connection', function (socketClient) {
-      socketClient.write(syslogFixtures);
-    });
+    ({server, socket} = await getServerWithFixtures(fixtures.SYSLOG_MESSAGES));
+    syslogService = new SyslogService(socket);
     await new B((resolve) => {
-      this.syslogService.start((log) => {
-        resolve(log);
-      });
+      syslogService.start(resolve);
     });
   });
 });
