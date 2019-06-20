22aa01254b0d08275985571d397456d2fb5d6697
umutuzgur <umutuzgur@gmail.com>
2019-06-20 15:53:15 +0200

    Add the syslog service for fetching the logs (#11)
    * Add the syslog service for fetching the logs
    
    * Changes after the PR review

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { default as Usbmux } from './lib/usbmux/usbmux';
-import { getConnectedDevices, getOSVersion, startLockdownSession } from './lib/utilities';
+import { getConnectedDevices, getOSVersion, startLockdownSession, startSyslogService, connectPort } from './lib/utilities';
 
 
-export { Usbmux, getConnectedDevices, getOSVersion, startLockdownSession };
+export { Usbmux, getConnectedDevices, getOSVersion, startLockdownSession, startSyslogService, connectPort };
 export default Usbmux;
