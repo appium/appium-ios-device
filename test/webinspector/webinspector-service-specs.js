481f5064e6ac783d4bb6c456ca369b17070e808e
Umut Uzgur <umutuzgur@gmail.com>
2019-06-22 13:02:23 +0200

    Make webinspector apis more rpc ready

--- test/webinspector/webinspector-service-specs.js
+++ test/webinspector/webinspector-service-specs.js
@@ -1,4 +1,5 @@
 import chai from 'chai';
+import B from 'bluebird';
 import WebInspectorService from '../../lib/webinspector';
 import { getServerWithFixtures, fixtures } from '../fixtures';
 
@@ -23,7 +24,9 @@ describe('webinspector', function () {
     ({server, socket} = await getServerWithFixtures(fixtures.WEBINSPECTOR_MESSAGES));
     webInspectorService = new WebInspectorService(socket);
     let obj = {__argument: {WIRConnectionIdentifierKey: '990cc163-d8b2-4d22-8d1c-644e100a5a07'}, __selector: '_rpc_reportIdentifier:'};
-    webInspectorService.sendPlist(obj);
-    await webInspectorService.receivePlist();
+    webInspectorService.sendMessage(obj);
+    await new B((resolve) => {
+      webInspectorService.listenMessage(resolve);
+    });
   });
 });
