d25248d5342090aa842fc84f6834df137c7c1835
umutuzgur <umutuzgur@gmail.com>
2019-06-24 14:38:50 +0200

    Refactor and add SSL enabled service support (#18)

--- test/utilities-specs.js
+++ test/utilities-specs.js
@@ -1,4 +1,4 @@
-import { getConnectedDevices, getOSVersion } from '..';
+import { utilities } from '..';
 import chai from 'chai';
 import { getServerWithFixtures, fixtures, UDID } from './fixtures';
 
@@ -17,14 +17,14 @@ describe('utilities', function () {
 
   it('should get unique udids', async function () {
     ({server, socket} = await getServerWithFixtures(fixtures.DEVICE_LIST));
-    const udids = await getConnectedDevices(socket);
+    const udids = await utilities.getConnectedDevices(socket);
     udids.length.should.be.equal(1);
     udids[0].should.eql(UDID);
   });
 
   it('should get product version', async function () {
     ({server, socket} = await getServerWithFixtures(fixtures.DEVICE_LIST, fixtures.DEVICE_CONNECT, fixtures.LOCKDOWN_GET_VALUE_OS_VERSION));
-    const osVersion = await getOSVersion(UDID, socket);
+    const osVersion = await utilities.getOSVersion(UDID, socket);
     osVersion.should.be.equal('12.3.1');
   });
 });
