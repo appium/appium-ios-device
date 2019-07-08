e36ca074ef98dfb16a302a964b9aa87cf1d4cf59
umutuzgur <umutuzgur@gmail.com>
2019-07-08 17:05:35 +0200

    fix: handle log message split better and fix split message transmission (#25)

--- test/syslog/syslog-service-specs.js
+++ test/syslog/syslog-service-specs.js
@@ -22,7 +22,29 @@ describe('syslog', function () {
     ({server, socket} = await getServerWithFixtures(fixtures.SYSLOG_MESSAGES));
     syslogService = new SyslogService(socket);
     await new B((resolve) => {
-      syslogService.start(resolve);
+      let count = 0;
+      syslogService.start((line) => {
+        // Send fake data to get more from the server
+        socket.write('fake');
+        if (++count === 3) {
+          resolve(line);
+        }
+      });
+    });
+  });
+
+  it('should wait for syslog split messages', async function () {
+    ({server, socket} = await getServerWithFixtures(fixtures.SYSLOG_SPLIT_MESSAGE_1, fixtures.SYSLOG_SPLIT_MESSAGE_2));
+    syslogService = new SyslogService(socket);
+    await new B((resolve) => {
+      let count = 0;
+      syslogService.start((line) => {
+        // Send fake data to get more from the server
+        socket.write('fake');
+        if (++count === 3) {
+          resolve(line);
+        }
+      });
     });
   });
 });
