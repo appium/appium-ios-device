06edff94273103c494a58bfc7c264d7246b80c84
Isaac A. Murchie <isaac.appium@gmail.com>
2019-11-12 14:50:34 -0500

    fix: send chunks of data to web inspector in real devices (#59)
    * fix: send chunks of data to web inspector
    
    * fix: only chunk data if real device
    
    * feat: allow chunk size to be passed in
    
    * refactor: use object not individual params
    
    * refactor: fix some issues with socket in web inspector
    
    * refactor: rename full hex dump option as verboseHexDump
    
    * address comments

--- test/webinspector/webinspector-service-specs.js
+++ test/webinspector/webinspector-service-specs.js
@@ -24,7 +24,14 @@ describe('webinspector', function () {
   it('should receive webinspector WIRFinalMessageKey messages back', async function () {
     const version = semver.coerce('10.2.0');
     ({server, socket} = await getServerWithFixtures(fixtures.WEBINSPECTOR_PARTIAL_MESSAGES));
-    webInspectorService = new WebInspectorService(version.major, false, false, socket);
+    webInspectorService = new WebInspectorService({
+      majorOsVersion: version.major,
+      isSimulator: true,
+      socketChunkSize: undefined,
+      verbose: true,
+      verboseHexDump: false,
+      socketClient: socket,
+    });
     let obj = {__argument: {WIRConnectionIdentifierKey: '990cc163-d8b2-4d22-8d1c-644e100a5a07'}, __selector: '_rpc_reportIdentifier:'};
     webInspectorService.sendMessage(obj);
     await new B((resolve) => {
@@ -34,7 +41,14 @@ describe('webinspector', function () {
   it('should receive webinspector ios 11 and above messages back', async function () {
     const version = semver.coerce('12.2.0');
     ({server, socket} = await getServerWithFixtures(fixtures.WEBINSPECTOR_MESSAGES));
-    webInspectorService = new WebInspectorService(version.major, false, false, socket);
+    webInspectorService = new WebInspectorService({
+      majorOsVersion: version.major,
+      isSimulator: true,
+      socketChunkSize: undefined,
+      verbose: true,
+      verboseHexDump: false,
+      socketClient: socket,
+    });
     let obj = {__argument: {WIRConnectionIdentifierKey: '990cc163-d8b2-4d22-8d1c-644e100a5a07'}, __selector: '_rpc_reportIdentifier:'};
     webInspectorService.sendMessage(obj);
     await new B((resolve) => {
