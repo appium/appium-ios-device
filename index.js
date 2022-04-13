bb541456711c6b641741cd2ef0ba35a0baabdbae
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2022-04-13 01:43:19 -0700

    fix typo

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import { default as Usbmux } from './lib/usbmux';
 import * as utilities from './lib/utilities';
 import * as services from './lib/services';
-import { SERVICE as INSTRUMENT_SERVICCE } from './lib/instrument';
+import { SERVICE as INSTRUMENT_SERVICE } from './lib/instrument';
 
 
-export { Usbmux, utilities, services, INSTRUMENT_SERVICCE };
+export { Usbmux, utilities, services, INSTRUMENT_SERVICE };
 export default Usbmux;
