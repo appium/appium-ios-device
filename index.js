2f2a182436e735e3c515ff27ee93531b6492db0b
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2022-04-13 01:40:25 -0700

    feat: expose SERVICE in instrument

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import { default as Usbmux } from './lib/usbmux';
 import * as utilities from './lib/utilities';
 import * as services from './lib/services';
+import { SERVICE as INSTRUMENT_SERVICCE } from './lib/instrument';
 
 
-export { Usbmux, utilities, services };
+export { Usbmux, utilities, services, INSTRUMENT_SERVICCE };
 export default Usbmux;
