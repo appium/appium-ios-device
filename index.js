40983e5d1a5e8042b036b8a1224088aabf4f181a
Kazuaki Matsuo <fly.49.89.over@gmail.com>
2022-04-14 15:50:02 +0900

    feat: expose SERVICE in instrument as INSTRUMENT_CHANNEL (#89)
    * feat: expose SERVICE in instrument
    
    * fix typo
    
    * rename

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 import { default as Usbmux } from './lib/usbmux';
 import * as utilities from './lib/utilities';
 import * as services from './lib/services';
+import { INSTRUMENT_CHANNEL } from './lib/instrument';
 
 
-export { Usbmux, utilities, services };
+export { Usbmux, utilities, services, INSTRUMENT_CHANNEL };
 export default Usbmux;
