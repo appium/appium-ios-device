ac44aac0ed175335fcee8e4df87d04b2572ddac0
tomriddly <tomriddly@qq.com>
2023-05-29 15:47:32 +0800

    fix: issue from code review.
    1. use promise.all for file stats check
    2. use chai-as-promised for reject test
    3. use zip inside test folder for download & unzip test
    
    Signed-off-by: tomriddly <tomriddly@qq.com>

--- test/imagemounter/list-developer-image-specs.js
+++ test/imagemounter/list-developer-image-specs.js
@@ -1,22 +1,25 @@
 import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
 import { findDeveloperImage } from '../../lib/imagemounter/utils/list_developer_image';
 import { fs } from '@appium/support';
 chai.should();
-describe('findDeveloperImage', function() {
-    it('should download and return the correct developer image for a given version', async function() {
-        const result = await findDeveloperImage('14.7.1', { githubRepo: 'JinjunHan/iOSDeviceSupport', subFolderList: ['iOSDeviceSupport'] });
+chai.use(chaiAsPromised);
+describe('findDeveloperImage', function () {
+    it('should download and return the correct developer image for a given version', async function () {
+        const result = await findDeveloperImage('14.7.1', {
+            githubRepo: 'appium/appium-ios-device',
+            subFolderList: ['test', 'imagemounter']
+        });
         result.developerImage.endsWith('/DeveloperDiskImage.dmg').should.be.true;
         result.developerImageSignature.endsWith('/DeveloperDiskImage.dmg.signature').should.be.true;
-        (await fs.exists(result.developerImage)).should.be.true;
-        (await fs.exists(result.developerImageSignature)).should.be.true;
+        fs.exists(result.developerImage).should.eventually.be.true;
+        fs.exists(result.developerImageSignature).should.eventually.be.true;
     });
 
-    it('should throw an error if the developer image cannot be found', async function() {
-        try {
-            await findDeveloperImage('99.99.99', { githubRepo: 'JinjunHan/iOSDeviceSupport', subFolderList: ['iOSDeviceSupport'] });
-            chai.assert.fail('Expected an error to be thrown');
-        } catch (err) {
-            err.message.should.equal('Failed to get developer image for iOS 99.99');
-        }
+    it('should throw an error if the developer image cannot be found', function () {
+        findDeveloperImage('99.99.99', {
+            githubRepo: 'appium/appium-ios-device',
+            subFolderList: ['test', 'imagemounter']
+        }).should.be.rejectedWith('Failed to get developer image for iOS 99.99');
     });
 });
