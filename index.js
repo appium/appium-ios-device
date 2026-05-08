4106f62510322d6e758deee863c1d41723fde27a
Mykola Mokhnach <mokhnach@gmail.com>
2026-05-08 17:45:56 +0200

    moar

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 export {default as Usbmux} from './lib/usbmux';
 export * as utilities from './lib/utilities';
 export * as services from './lib/services';
-export { INSTRUMENT_CHANNEL } from './lib/instrument';
-export { TESTMANAGERD_CHANNEL } from './lib/testmanagerd';
-export { Xctest } from './lib/xctest';
+export {INSTRUMENT_CHANNEL} from './lib/instrument';
+export {TESTMANAGERD_CHANNEL} from './lib/testmanagerd';
+export {Xctest} from './lib/xctest';
 export {default} from './lib/usbmux';
